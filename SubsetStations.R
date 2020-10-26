cf151d54029048e8155ab632c6726140dedae7a3
dblaskey <dylan.blaskey@colorado.edu>
2020-10-26 07:36:08 -0600

    Add Flashiness

--- SubsetStations.R
+++ SubsetStations.R
@@ -1,5 +1,5 @@
 library(pacman)
-pacman::p_load(dataRetrieval, dplyr, tidyverse, leaflet)
+pacman::p_load(dataRetrieval, tidyverse)
 
 sites <- dataset
 
@@ -9,8 +9,8 @@ Summarized_data <- sites %>%
   summarise(start_date = min(Date), end_date=max(Date), observations=max(X))
 
 Summarized_data <- Summarized_data %>%
-  mutate(end_date=as.POSIXct(end_date,tz="AST")) %>%
-  mutate(start_date=as.POSIXct(start_date,tz="AST")) %>%
+  mutate(end_date=as.POSIXct(end_date)) %>%
+  mutate(start_date=as.POSIXct(start_date)) %>%
   mutate(Record_length = (end_date - start_date)/365.25) %>%
   mutate(Completeness = (observations/365.25)/as.double(Record_length)) %>%
   mutate(Effective_obs = Completeness*Record_length)
@@ -30,11 +30,43 @@ obsm <- sites2 %>%
 #Set criteria for passing
 Ycriteria <- 0.9*365
 Mcriteria <- 0.8*30
+MinYcriteria <- 11
 
-#Analyize what passes this criteria
-obsy %>%
+#Analyze what passes this criteria
+obsy <- obsy %>%
   mutate(Ypass = ifelse(Yearly_obs>Ycriteria,1,0))
-obsm %>%
+obsm <- obsm %>%
   mutate(Mpass = ifelse(Monthly_obs>Mcriteria,1,0))
 
+pass_month <- obsm %>%
+  group_by(site_no, Year) %>%
+  summarise(Months = sum(Mpass))
+
+pass_year <- left_join(obsy, pass_month)
+
+fail <- pass_year %>%
+  filter(Months<MinYcriteria | Ypass==0)
+
+#Remove lines that fail criteria
+final <- anti_join(sites2,fail) %>%
+  mutate(date=as.POSIXct(paste(Year, Month, Day, sep="-")))
+
+#Monthly and yearly discharge
+qy <- final %>%
+  group_by(site_no, Year) %>%
+  summarise(Annual_Discharge = mean(X_00060_00003))
+
+qm <- final %>%
+  group_by(site_no, Year, Month) %>%
+  summarise(Monthly_Discharge = mean(X_00060_00003))
+
+#Flashiness
+flash <- final %>%
+  group_by(site_no) %>%
+  mutate(dq = ifelse(date-lag(date,default = date[1])>0 ,abs(X_00060_00003-lag(X_00060_00003)),NA))
+
+flashiness <- flash %>%
+  group_by(site_no, Year, Month) %>%
+  summarise(sum_dq = sum(dq, na.rm=TRUE), sum_q = sum(X_00060_00003)) %>%
+  mutate(dq_q = ifelse(sum_q>0, sum_dq/sum_q, NA))
 
